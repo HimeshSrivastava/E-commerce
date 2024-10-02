6c7d8a84f1d2414687c50b3df95f00b1970d0d18
HimeshSrivastava <153629881+HimeshSrivastava@users.noreply.github.com>
2024-10-02 22:21:48 +0530

    getting ready

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import './Hero.css'
+// import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-      <div className="hero-left">
+    <div className='flex gap-7 text-6xl font-extrabold items-center justify-center'>
+      <div className="flex flex-col gap-8 items-center justify-center">
        <h2>NEW ARRIVALS ONLY</h2>
        <div>
-        <div className="hero-hand-icon">
+        <div className="flex gap-4">
             <p>New</p>
-            <img src="https://i.pinimg.com/originals/c8/2d/de/c82dde2e0b94cdb3fe7c9a56919ef154.gif" alt="hii" />
+            <img className='w-28 h-20'  src="https://i.pinimg.com/originals/c8/2d/de/c82dde2e0b94cdb3fe7c9a56919ef154.gif" alt="hii" />
         </div>
-        <p>Collections</p>
+        <p >Collections</p>
         <p>for everyone</p>
        </div>
-       <div className="hero-lastest-btn">
+       <div className="bg-red-500 text-white rounded-sm text-4xl">
         Lastest Collection
         <img src="" alt="" />
        </div>
       </div>
-      <div className="hero-right">
+      <div className="flex flex-col gap-8 items-center justify-center">
       <img src="https://www.pngmart.com/files/15/Beautiful-Girl-Holding-Shopping-Bag-Transparent-PNG.png" alt="" />
       </div>
     </div>
