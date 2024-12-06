134d71b8b6c5709487f64dfdde73ba3016674a8c
HimeshSrivastava <153629881+HimeshSrivastava@users.noreply.github.com>
2024-12-06 19:35:13 +0530

    backend url

--- backend/Index.js
+++ backend/Index.js
@@ -47,7 +47,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://e-commerce-1-px4m.onrender.com/images/${req.file.filename}`,
+    image_url: `https://e-commerce-zq42.onrender.com/images/${req.file.filename}`,
   });
 });
 
